299f4a6c4521e21bcad6c79562ab96612f6aec96
Offklaus <joaovictorsrs2002@gmail.com>
2025-12-09 20:42:50 -0300

    correção route /animais

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import Doar from "./pages/Doar";
 import AdoptionList from "./pages/AdoptionsList";
 import AnimalDetail from "./pages/AnimalDetail";
+import {AnimalProvider} from "./context/AnimalContext";
 import "./index.css";
 
 
 export default function App() {
   return (
+    <AnimalProvider>
     <>
       <Navbar />
 
@@ -17,11 +19,12 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/doar" element={<Doar />} />
-          <Route path="/adocao" element={<AdoptionList />} />
+          <Route path="/animais" element={<AdoptionList />} />
           <Route path="/animais" element={<AnimalDetail />} />
         </Routes>
       </div>
     </>
+    </AnimalProvider>
   );
 }
 
